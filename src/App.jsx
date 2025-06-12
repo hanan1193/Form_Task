f5eb46f44b1f73b407cca15b838308d1237e3f49
hanan <hanan.nairat13@gmail.com>
2025-06-12 13:18:10 +0300

    Fix GitHub Pages routing with basename and App routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
-import WelcomePage from './pages/WelcomePage.jsx';
+import { Routes, Route } from 'react-router-dom'
+import WelcomePage from './pages/WelcomePage.jsx'
 import PageNotFound from './pages/PageNotFound.jsx'
-import AddPatientFormikForm from './pages/AddPatientFormikForm.jsx'; 
-import AddPatientHookForm from './pages/AddPatientHookForm.jsx';
+import AddPatientFormikForm from './pages/AddPatientFormikForm.jsx'
+import AddPatientHookForm from './pages/AddPatientHookForm.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/hook" element={<AddPatientHookForm />} />
-        <Route path="/formik" element={<AddPatientFormikForm />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
-export default App;
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<WelcomePage />} />
+      <Route path="/hook" element={<AddPatientHookForm />} />
+      <Route path="/formik" element={<AddPatientFormikForm />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  )
+}
